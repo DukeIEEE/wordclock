5eaa6f22e1bb930bf51dbf321ffa8e3c0d315796
Qian Wang <qwang2012@gmail.com>
2016-03-02 17:50:23 -0500

    Moved wordclock.ino to wordclock folder to open in Arduino

--- firmware/wordclock/wordclock.ino
+++ firmware/wordclock/wordclock.ino
@@ -1,3 +1,4 @@
+
 // Wordclock firmware
 // ==================
 // November 2013 - August 2014
@@ -10,43 +11,43 @@
 
 /* Hardware block diagram:
 
-              +-----------------+                            
-              | Real time clock |                            
-              | Maxim DS3231    |                            
-              +--------+--------+                            
-                       |I2C                                  
-         +-------------+-------------+                       
+              +-----------------+
+              | Real time clock |
+              | Maxim DS3231    |
+              +--------+--------+
+                       |I2C
+         +-------------+-------------+
          |                           |   +------------------+
          |                           +---+ 8x8 LED matrix 1 |
-+---+    |                           |   | Maxim MAX7219    |
-|LDR+----+                           |   +---------+--------+
-+---+    |      Microcontroller      |             |         
+  +---+    |                           |   | Maxim MAX7219    |
+  |LDR+----+                           |   +---------+--------+
+  +---+    |      Microcontroller      |             |
          |      Atmel ATMEGA328      |   +---------+--------+
-+------+ |      (with Arduino        |   | 8x8 LED matrix 2 |
-|Buzzer+-+       bootloader)         |   | Maxim MAX7219    |
-+------+ |                           |   +---------+--------+
-         |                           |             |         
+  +------+ |      (with Arduino        |   | 8x8 LED matrix 2 |
+  |Buzzer+-+       bootloader)         |   | Maxim MAX7219    |
+  +------+ |                           |   +---------+--------+
+         |                           |             |
          |                           |   +---------+--------+
          +-++----++---------++----++-+   | 8x8 LED matrix 3 |
            ||    ||         ||    ||     | Maxim MAX7219    |
     +------++-+  ||  +------++-+  ||     +---------+--------+
-    | Azoteq  |  ||  | Azoteq  |  ||               |         
+    | Azoteq  |  ||  | Azoteq  |  ||               |
     | IQS127D |  ||  | IQS127D |  ||     +---------+--------+
     +---------+  ||  +---------+  ||     | 8x8 LED matrix 4 |
                  ||               ||     | Maxim MAX7219    |
           +------++-+      +------++-+   +------------------+
-          | Azoteq  |      | Azoteq  |                       
-          | IQS127D |      | IQS127D |                       
-          +---------+      +---------+                       
+          | Azoteq  |      | Azoteq  |
+          | IQS127D |      | IQS127D |
+          +---------+      +---------+
 
-(created using http://asciiflow.com/) */
+  (created using http://asciiflow.com/) */
 
 
 // Includes
 #include "LedControl.h" // MAX7219 - LED matrix drivers - http://playground.arduino.cc/Main/LedControl
 #include "Chronodot.h"  // DS3231  - Real time clock    - https://github.com/Stephanie-Maks/Arduino-Chronodot
 #include <Wire.h>
-#include <EEPROM.h> 
+#include <EEPROM.h>
 
 // Pins to capacitive touch chips (touch and presence for each of the Azoteq IQS127D chips in the four corners)
 const int pinTRB = 9;   // Touch Right Bottom
@@ -81,7 +82,7 @@ LedControl lc = LedControl(pinData, pinClock, pinLoad, noBoards);
 // The real time clock chip (DS3231)
 Chronodot RTC;
 
-// LED grid 
+// LED grid
 const int bs = 8;
 const int bss = 3;
 const int boards[2][2] = { {2, 1}, {3, 0} };
@@ -90,9 +91,9 @@ const int boards[2][2] = { {2, 1}, {3, 0} };
 const int wait = 10;
 const int noTasks = 3;
 typedef struct Tasks {
-   long unsigned int previous;
-   int interval;
-   void (*function)();
+  long unsigned int previous;
+  int interval;
+  void (*function)();
 } Task;
 Task tasks[noTasks];
 
@@ -108,7 +109,7 @@ boolean prevframe[16][16];
 
 const int w_it[3] =        { 0,  0,  2 };
 const int w_is[3] =        { 0,  3,  2 };
-const int w_half[3] =      { 7,  0,  4 }; 
+const int w_half[3] =      { 7,  0,  4 };
 const int w_to[3] =        { 7,  14, 2 };
 const int w_past[3] =      { 8,  0,  4 };
 const int w_oclock[3] =    { 12, 0, 6 };
@@ -126,6 +127,11 @@ const int w_duke[3] =      {15, 0, 4  };
 const int w_ieee[3] =      {15, 5, 4};
 const int w_time[3] =      {15, 10, 5};
 const int w_el[3] =        { 9,  2,  2 };
+const int w_cold[3] =      {15, 0, 4  }; // do we need these?
+const int w_cool[3] =      {15, 5, 4}; // do we need these?
+const int w_warm[3] =      {15, 10, 5}; // do we need these?
+const int w_hot[3] =        { 9,  2,  2 }; // do we need these?
+
 
 const int w_minutes[20][3] = {
   { 0,  13, 3 }, // one
@@ -172,18 +178,18 @@ boolean tlb;
 boolean trb;
 
 void setup() {
-  
+
   // Debug info
   Serial.begin(9600);
   Serial.println("[INFO] Wordclock is booting...");
-  
+
   // Read settings from EEPROM
   Serial.println("[INFO] 1. Read settings");
   brightness = EEPROM.read(0);
-  
+
   // Initiate the LED drivers
   Serial.println("[INFO] 2. LED drivers");
-  for(int i = 0; i < noBoards; ++i) {
+  for (int i = 0; i < noBoards; ++i) {
     lc.shutdown(i, false);
     lc.clearDisplay(i);
   }
@@ -197,7 +203,7 @@ void setup() {
   //  Serial.println("[WARNING] RTC is NOT running!");
   //  RTC.adjust(DateTime(__DATE__, __TIME__));
   //}
-  
+
   // Initiate the capacitive touch inputs
   Serial.println("[INFO] 4. Capacitive touch");
   pinMode(pinTRB, INPUT);
@@ -208,24 +214,24 @@ void setup() {
   //pinMode(pinPRT, INPUT);
   //pinMode(pinPLT, INPUT);
   //pinMode(pinPLB, INPUT);
-  
+
   // Tasks
   Serial.println("[INFO] 5. Tasks");
   loadTasks();
-  
+
   // Debug info
   Serial.println("[INFO] Wordclock done booting. Hellooooo!");
   printMenu();
-  
+
 }
 
 void loadTasks() {
-  
+
   // Listen for input on the serial interface
   tasks[0].previous = 0;
   tasks[0].interval = 100;
   tasks[0].function = serialMenu;
-  
+
   // Show time
   tasks[1].previous = 0;
   tasks[1].interval = 1000;
@@ -235,26 +241,26 @@ void loadTasks() {
   tasks[2].previous = 0;
   tasks[2].interval = 100;
   tasks[2].function = readTouch;
-  
+
 }
 
 void loop() {
   unsigned long time = millis();
-  for(int i = 0; i < noTasks; i++) {
+  for (int i = 0; i < noTasks; i++) {
     Task task = tasks[i];
     if (time - task.previous > task.interval) {
       tasks[i].previous = time;
       task.function();
     }
-  }  
+  }
   delay(wait);
 }
 
 void serialMenu() {
   if (Serial.available() > 0) {
-    if(mustReadBrightness) {
+    if (mustReadBrightness) {
       int val = Serial.parseInt();
-      if(val < 0 || val > 15) {
+      if (val < 0 || val > 15) {
         Serial.println("[ERROR] Brightness must be between 0 and 15");
       } else {
         Serial.print("Brightness set to ");
@@ -285,35 +291,35 @@ void serialMenu() {
 }
 
 void showTime() {
-  
+
   // Get the time
-  DateTime now = RTC.now();  
+  DateTime now = RTC.now();
   int h = now.hour();
   int h2 = h;
   int m = now.minute();
   int t = now.tempC();
-  
+
   // DEBUG
   /*Serial.print("[DEBUG] ");
-  Serial.print(h, DEC);
-  Serial.print(':');
-  Serial.println(m, DEC);*/
-  
+    Serial.print(h, DEC);
+    Serial.print(':');
+    Serial.println(m, DEC);*/
+
   // The frame
   boolean frame[16][16];
-  for(int r = 0; r < 16; r++) {
-    for(int c = 0; c < 16; c++) {
+  for (int r = 0; r < 16; r++) {
+    for (int c = 0; c < 16; c++) {
       frame[r][c] = false;
     }
   }
- 
+
   // Show "IT IS"
   addWordToFrame(w_it, frame);
   addWordToFrame(w_is, frame);
-  
+
   // Minutes
   if (m == 0) {
-    
+
     if (h == 0) {
       addWordToFrame(w_midnight, frame);
     } else if (h == 12) {
@@ -323,7 +329,7 @@ void showTime() {
     }
 
   } else {
-  
+
     if (m <= 20) {
       addWordToFrame(w_minutes[m - 1], frame);
     } else if (m < 30) {
@@ -337,39 +343,39 @@ void showTime() {
     } else {
       addWordToFrame(w_minutes[60 - m - 1], frame);
     }
- 
-    if(m <= 30) {
+
+    if (m <= 30) {
       addWordToFrame(w_past, frame);
     } else {
       addWordToFrame(w_to, frame);
       ++h2;
     }
-    
-  } 
-  
-  if(!(m ==0 && (h == 0 || h == 12))) {
-  
+
+  }
+
+  if (!(m == 0 && (h == 0 || h == 12))) {
+
     // Hours
-    if(h2 == 0) {
+    if (h2 == 0) {
       addWordToFrame(w_hours[11], frame);
     } else if (h2 <= 12) {
       addWordToFrame(w_hours[h2 - 1], frame);
     } else {
       addWordToFrame(w_hours[h2 - 13], frame);
     }
-    if(h2 == 11 || h2 == 23) {
+    if (h2 == 11 || h2 == 23) {
       addWordToFrame(w_el, frame);
     }
-  
+
     // Time of day
-    if(h < 6) {
+    if (h < 6) {
       addWordToFrame(w_at, frame);
       addWordToFrame(w_night, frame);
-    } else if(h < 12) {
+    } else if (h < 12) {
       addWordToFrame(w_in, frame);
       addWordToFrame(w_the, frame);
       addWordToFrame(w_morning, frame);
-    } else if(h < 18) {
+    } else if (h < 18) {
       addWordToFrame(w_in, frame);
       addWordToFrame(w_the, frame);
       addWordToFrame(w_afternoon, frame);
@@ -378,19 +384,19 @@ void showTime() {
       addWordToFrame(w_night, frame);
     }
   }
-  
+
   // Duke IEEE Time! (2 PM to 5 PM)
-  int weekday=now.dayOfWeek();
-  
-  if (weekday=6 && 14 < h && h < 17) {
+  int weekday = now.dayOfWeek();
+
+  if (weekday = 6 && 14 < h && h < 17) {
     addWordToFrame(w_duke, frame);
     addWordToFrame(w_ieee, frame);
     addWordToFrame(w_time, frame);
   }
-  
+
   // Temperature
   addWordToFrame(w_and, frame);
-  if(t <= 16) {
+  if (t <= 16) {
     addWordToFrame(w_cold, frame);
   } else if (t <= 20) {
     addWordToFrame(w_cool, frame);
@@ -402,14 +408,14 @@ void showTime() {
 
   // Update display
   updateDisplay(prevframe, frame);
-  
+
   // Copy current frame to buffer
-  for(int r = 0; r < 16; r++) {
-    for(int c = 0; c < 16; c++) {
+  for (int r = 0; r < 16; r++) {
+    for (int c = 0; c < 16; c++) {
       prevframe[r][c] = frame[r][c];
     }
   }
-  
+
 }
 
 void readTouch() {
@@ -417,14 +423,14 @@ void readTouch() {
   boolean rt = debounce(digitalRead(pinTRT) == LOW, &trt);
   boolean lb = debounce(digitalRead(pinTLB) == LOW, &tlb);
   boolean rb = debounce(digitalRead(pinTRB) == LOW, &trb);
-  if(lt || rt || lb || rb) {
+  if (lt || rt || lb || rb) {
     tone(pinBuzzer, 500, 100);
   }
 }
 
 boolean debounce(boolean value, boolean* store) {
-  if(value) {
-    if(*store) {
+  if (value) {
+    if (*store) {
       value = false;
     } else {
       *store = true;
@@ -436,8 +442,8 @@ boolean debounce(boolean value, boolean* store) {
 }
 
 void setAllLeds(boolean on) {
-  for(int i = 0; i < noBoards; ++i) {
-    for(int r = 0; r < 8; ++r) {
+  for (int i = 0; i < noBoards; ++i) {
+    for (int r = 0; r < 8; ++r) {
       lc.setRow(i, r, B11111111);
     }
   }
@@ -449,8 +455,8 @@ void setLed(int row, int col, boolean on) {
   int board = boards[t][l];
   int r = row % bs;
   int c = col % bs;
-  if(t == 0) {
-    if(l == 0) {
+  if (t == 0) {
+    if (l == 0) {
       r = bs - 1 - r;
       c = (c + 1) % bs;
     } else {
@@ -459,7 +465,7 @@ void setLed(int row, int col, boolean on) {
       c = (r1 + 1) % bs;
     }
   } else {
-    if(l == 0) {
+    if (l == 0) {
       int r1 = r;
       r = bs - 1 - c;
       c = (bs - r1) % bs;
@@ -471,25 +477,25 @@ void setLed(int row, int col, boolean on) {
 }
 
 void updateDisplay(boolean previousframe[16][16], boolean frame[16][16]) {
-  for(int r = 0; r < 16; ++r) {
-    for(int c = 0; c < 16; ++c) {
-      if(prevframe[r][c] && !frame[r][c]) {
+  for (int r = 0; r < 16; ++r) {
+    for (int c = 0; c < 16; ++c) {
+      if (prevframe[r][c] && !frame[r][c]) {
         setLed(r, c, false);
-      } else if(!prevframe[r][c] && frame[r][c]) {
+      } else if (!prevframe[r][c] && frame[r][c]) {
         setLed(r, c, true);
       }
     }
   }
 }
 
-void addWordToFrame(const int theword[3], boolean frame[16][16]){
-  for(int i = 0; i < theword[2]; ++i) {
+void addWordToFrame(const int theword[3], boolean frame[16][16]) {
+  for (int i = 0; i < theword[2]; ++i) {
     frame[theword[0]][theword[1] + i] = true;
   }
 }
 
 void setBrightness(int value) {
-  for(int i = 0; i < noBoards; ++i) {
+  for (int i = 0; i < noBoards; ++i) {
     lc.setIntensity(i, value);
   }
 }
